2d020dbd8866601e7c02fc0e8991ffeebad091c3
ngtien137 <ngtien137@gmail.com>
2024-05-08 23:54:32 +0700

    init

---
+++ Assets/MainDemoController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using WASticker;
+
+public class MainDemoController : MonoBehaviour
+{
+    [SerializeField]
+    private Text textStatus;
+
+    [SerializeField]
+    private WAStickerModule wAStickerModule;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+#if UNITY_ANDROID && !UNITY_EDITOR
+    void Update()
+    {
+        textStatus.text = $"Library Status: {wAStickerModule.GetLibraryStatus()}";
+    }
+#endif
+    string stickerIdentifier = $"5";
+    string stickerPackName = "StickerPack 5";
+    string[] stickerPaths;
+    string trayFile = "file_sticker_tray.png";
+    public void ClickSetSticker()
+    {
+        wAStickerModule.ApplyStickerToWhatsapp(stickerIdentifier, stickerPackName);
+        wAStickerModule.ToastMessage("Set");
+    }
+
+    public void ClickGenerateImage()
+    {
+        string stickerFileName = "file_sticker.webp";
+        GenerateStickerOrTray("sticker", stickerFileName, 0);
+        GenerateStickerOrTray("sticker_tray", trayFile, 1);
+        stickerPaths = new string[] { stickerFileName, stickerFileName, stickerFileName };
+        wAStickerModule.SetStickerInformation(stickerIdentifier, stickerPackName, "TienUU", "1", trayIconPath: trayFile, stickerPaths);
+        wAStickerModule.ToastMessage("Generated");
+    }
+
+    private IEnumerator Wait(string stickerIdentifier, string stickerPackName)
+    {
+        yield return new WaitForSeconds(2);
+        wAStickerModule.ApplyStickerToWhatsapp(stickerIdentifier, stickerPackName);
+        wAStickerModule.ToastMessage("Set");
+    }
+
+    public void GenerateStickerOrTray(string resourcesPath, string fileName, int trayOrSticker) // 1 is tray, 0 is sticker
+    {
+        
+        // Đường dẫn đến thư mục internal của ứng dụng
+        string internalPath = Application.persistentDataPath;
+
+        // Đọc nội dung của file từ thư mục Resources
+        byte[] fileBytes = null;
+
+        Texture2D texture = Resources.Load<Texture2D>(resourcesPath);
+        Texture2D decompressTexture = DeCompress(texture);
+        if (trayOrSticker == 0)
+        {
+            wAStickerModule.SaveImageAsSticker(decompressTexture, fileName, 90);
+        }
+        else
+        {
+            wAStickerModule.SaveImageAsTray(decompressTexture, fileName, 90);
+        }
+    }
+
+    public Texture2D DeCompress(Texture2D source)
+    {
+        RenderTexture renderTex = RenderTexture.GetTemporary(
+                    source.width,
+                    source.height,
+                    0,
+                    RenderTextureFormat.Default,
+                    RenderTextureReadWrite.Linear);
+
+        Graphics.Blit(source, renderTex);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = renderTex;
+        Texture2D readableText = new Texture2D(source.width, source.height);
+        readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        readableText.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTex);
+        return readableText;
+    }
+}
---
+++ Assets/WASticker/WAStickerModule.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace WASticker
+{
+    public class WAStickerModule : MonoBehaviour
+    {
+        const string pluginName = "com.lemon.wasticker.MainPlugin";
+
+        private static AndroidJavaClass _pluginClass;
+        private static AndroidJavaObject _pluginInstance;
+        private static AndroidJavaObject currentActivity;
+
+        public static AndroidJavaClass PluginClass
+        {
+            get
+            {
+                if (_pluginClass == null)
+                {
+                    _pluginClass = new AndroidJavaClass(pluginName);
+                }
+                return _pluginClass;
+            }
+        }
+
+        public static AndroidJavaObject PluginInstance
+        {
+            get
+            {
+                if (_pluginInstance == null)
+                {
+                    _pluginInstance = PluginClass.CallStatic<AndroidJavaObject>("getInstance");
+                }
+                return _pluginInstance;
+            }
+        }
+
+        private static AndroidJavaObject GetCurrentActivity()
+        {
+            if (currentActivity == null)
+            {
+                AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            }
+            return currentActivity;
+        }
+
+        void Start()
+        {
+        }
+
+        public int GetLibraryStatus()
+        {
+            //just demo
+            int status = 0;
+            try
+            {
+                status = PluginInstance.Call<int>("getPluginStatus");
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("Exception " + e);
+                status = -1;
+            }
+
+            return status;
+
+        }
+
+        public T CallFunctionFromPlugin<T>(string functionName, params object[] parameters)
+        {
+            return PluginInstance.Call<T>(functionName, parameters);
+        }
+
+        public void CallFunctionFromPlugin(string functionName, params object[] parameters)
+        {
+            PluginInstance.Call(functionName, parameters);
+        }
+
+        public void ToastMessage(string message)
+        {
+            CallFunctionFromPlugin("toastMessage", GetCurrentActivity(), message);
+        }
+
+        public void SetStatusBarTransparent()
+        {
+            CallFunctionFromPlugin("setStatusBarTransparent", GetCurrentActivity());
+        }
+
+        public void SetStickerInformation(string identifier, string stickerPackName, string publisher, string imageDataVersion, string trayIconPath, params string[] stickersPath)
+        {
+            CallFunctionFromPlugin("SetStickerInformation",GetCurrentActivity() , identifier, stickerPackName, publisher, imageDataVersion, trayIconPath, StickerPathToJson(stickersPath));
+        }
+
+        public bool ApplyStickerToWhatsapp(string identifier, string stickerPackName)
+        {
+            return CallFunctionFromPlugin<bool>("ApplyStickerFromActivity", GetCurrentActivity(), identifier, stickerPackName);
+        }
+
+        public string GetStickerFolder()
+        {
+            return CallFunctionFromPlugin<string>("getStickerFolder", GetCurrentActivity());
+        }
+
+        public string StickerPathToJson(params string[] values)
+        {
+            return JsonConvert.SerializeObject(values);
+
+        }
+
+        public void SaveImageAsSticker(Texture2D texture, string stickerName, int quality)
+        {
+            //Quality should be 90
+            CallFunctionFromPlugin("SaveImageAsSticker", GetCurrentActivity(), GetSByteFromTexture(texture), stickerName, quality);;
+        }
+
+        public void SaveImageAsTray(Texture2D texture, string stickerName, int quality)
+        {
+            //Quality should be 90
+            CallFunctionFromPlugin("SaveImageAsTray", GetCurrentActivity(), GetSByteFromTexture(texture), stickerName, quality);
+        }
+
+        private sbyte[] GetSByteFromTexture(Texture2D sourceTexture)
+        {
+            byte[] pixels = sourceTexture.EncodeToPNG();
+
+            // Chuyển mảng byte sang mảng sbyte
+            sbyte[] sPixels = new sbyte[pixels.Length];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                sPixels[i] = (sbyte)pixels[i];
+            }
+            return sPixels;
+        }
+    }
+
+}
